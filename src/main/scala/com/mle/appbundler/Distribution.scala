ee7d0cfbddb4de5ad96cdbd60f5ae3ec9a3fe531
Michael Skogberg <malliina123@gmail.com>
2014-12-11 13:13:48 +0200

    refac

--- src/main/scala/com/mle/appbundler/Distribution.scala
+++ src/main/scala/com/mle/appbundler/Distribution.scala
@@ -18,21 +18,17 @@ object Distribution extends XmlWriter {
    * @return
    */
   def xml(conf: DistributionConf): Node = {
-//    val org = conf.organization
     val appID = conf.appIdentifier
     val daemonName = s"$appID.daemon"
     val displayName = conf.displayName
     val pkgName = s"${conf.name}.pkg"
-    //      <welcome file="welcome.html" mime-type="text/html"/>
-    //      <license file="license.html" mime-type="text/html"/>
-    //      <conclusion file="conclusion.html" mime-type="text/html"/>
 
     <installer-gui-script minSpecVersion="1">
       <title>{displayName}</title>
       <organization>{appID}</organization>
       <domains enable_localSystem="true"/>
       <options customize="never" require-scripts="true" rootVolumeOnly="true"/>
-      <!-- For exmaple define documents displayed at various steps -->
+      <!-- For example define documents displayed at various steps -->
       {conf.additionalXml}
       <!-- List all component packages -->
       <pkg-ref id={daemonName}
