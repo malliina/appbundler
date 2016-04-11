e878f007b63bb13c18f45574ee70081de174735e
Michael Skogberg <michael.skogberg@outlook.com>
2016-04-11 23:34:37 +0300

    Update deps

--- project/AppBundlerBuild.scala
+++ project/AppBundlerBuild.scala
@@ -9,12 +9,12 @@ object AppBundlerBuild extends Build {
     .settings(projectSettings: _*)
 
   lazy val projectSettings = Seq(
-    version := "0.9.0",
+    version := "0.9.1",
     organization := "com.malliina",
     scalaVersion := "2.11.7",
     crossScalaVersions := Seq("2.10.6", scalaVersion.value),
     fork in Test := true,
-    libraryDependencies ++= Seq("com.malliina" %% "util" % "2.1.0"),
+    libraryDependencies ++= Seq("com.malliina" %% "util" % "2.2.0"),
     updateOptions := updateOptions.value.withCachedResolution(true),
     licenses +=("MIT", url("http://opensource.org/licenses/MIT"))
   )
